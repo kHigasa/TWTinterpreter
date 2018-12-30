da33895826f285c21b5e1a44f49019c950afde37
kHigasa <kojihigasa@gmail.com>
2018-12-30 16:26:24 +0900

    tokenize bracket

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,7 +2,6 @@ package lexer
 
 import (
 	"testing"
-
 	"../token"
 )
 
@@ -28,6 +27,7 @@ if (5 < 10) {
 10 != 9;
 "foobar"
 "foo bar"
+[1, 2];
 `
 
 	tests := []struct {
@@ -109,6 +109,12 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
