3f1eaf96d1877f5b7c95b21215c7dcafb21c3aba
kHigasa <kojihigasa@gmail.com>
2018-12-30 17:12:17 +0900

    eval array literal

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -312,6 +312,20 @@ func TestStringLiteral(t *testing.T) {
 	}
 }
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func TestBuiltinFunctions(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -343,18 +357,22 @@ func TestBuiltinFunctions(t *testing.T) {
 	}
 }
 
-func TestStringConcatenation(t *testing.T) {
-	input := `"Hello" + " " + "World!"`
+func TestArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
 
 	evaluated := testEval(input)
-	str, ok := evaluated.(*object.String)
+	result, ok := evaluated.(*object.Array)
 	if !ok {
-		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
 	}
 
-	if str.Value != "Hello World!" {
-		t.Errorf("String has wrong value. got=%q", str.Value)
+	if len(result.Elements) != 3 {
+		t.Fatalf("array has wrong num of elements. got=%d", len(result.Elements))
 	}
+
+	testIntegerObject(t, result.Elements[0], 1)
+	testIntegerObject(t, result.Elements[1], 4)
+	testIntegerObject(t, result.Elements[2], 6)
 }
 
 func testEval(input string) object.Object {
